8531122257f1ce63f1124757b022e4c15fd1e2fa
Asad-ullah-turab <asadullahturab47@gmail.com>
2024-01-29 18:44:31 +0500

    add cmake

---
+++ build/CMakeFiles/PICTURE-TO-ASCII.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PICTURE-TO-ASCII.dir/main.cpp.o"
+  "CMakeFiles/PICTURE-TO-ASCII.dir/main.cpp.o.d"
+  "PICTURE-TO-ASCII"
+  "PICTURE-TO-ASCII.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PICTURE-TO-ASCII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PICTURE-TO-ASCII.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PICTURE-TO-ASCII.
+# This may be replaced when dependencies are built.
